3a9d1e462cf4e6d8a80b27070d708c23e9f73c83
Oceane <oceanelonneux@gmail.com>
2018-08-14 22:17:00 +0100

    have to do two player with high score

--- Play/play.js
+++ Play/play.js
@@ -4,10 +4,12 @@ $(function (event){
       //MONSTER PART
   var container = ["0"]; //i only have one picture now
   var img = $(".img");
+  var restart = $(".restart");
   var highScore = 0; //highscore
   //generate random position values for top
   function generateRandomForArray() {
-    var num = Math.floor(Math.random() * 2);
+    var num = Math.floor(Math.random() * 4);
+    //put all the images in img folder and name them 0 1 2 3
     return num;
   }
   function generateRandom() {
@@ -32,30 +34,28 @@ $(function (event){
   var playGame = false;
   var playerScore = 0; //the score of the player at the beginning
   var turn = 0; //who is playing
-  //i want to store the score of each player, compare them after
-  // timer
-
-
+  //i want to store the score of each player, compare them after timer
   var timeLeft = 20; //Im giving the player 40s to play
   //time out during the play
   var timer = setInterval(function(){
     if (timeLeft != 0) {
       timeLeft--;
+      $(".playerScore").html(playerScore);
       //I want to take the number of score and record it
       $("score").each(function(index){
         $(this).data("score", index);
       })
 
+
       //Clicking on the body
         // I want the bat to rotate
         //I want the monster to disappear
         //ONLY ONE CLICK FUNCTION
-        $(".enemy").click(function() {
+        $(".img").click(function() {
           console.log("hello")
-          playerScore++;
+          playerScore++; //raise score
           score.html("<b  class='score'> Score: " + playerScore + " </b>");
           // setTimeout(myFunction, 300);
-          $(".enemy").remove();
         //IF on monster : add +1 to the score, else: score stays the same
           playerScore++;
           score.html("<b  class='score'> Score: " + playerScore + " </b>");
@@ -63,13 +63,26 @@ $(function (event){
     }
     else if (timeLeft == 0){
       $(".img img:last-child").remove() //remove image of last enemy
+      // STORE THE SCORES//
       console.log("finish");
       //TO DO: close the game when finished
       playGame = false;
-      clearInterval(timer);
-      /////its doing again and again the count!!!
+      restart.click(function() {
+        location.reload(); //reload page
+      })
+      var timerId = 0;
+      function setResetInterval(bool) {
+        if (bool) {
+          timerId = setInterval(function() {
+            setValue();
+          }, 1000)
+        }
+      }
+      clearInterval(timerId);
     }//end of else if
-  }, 1000);
+  }, 1000); //end of set interval
+
+
 
 // make an array of [0 1 2]
 // randomArray to pick from the array inside the setinterval function so it calls a random
@@ -78,6 +91,7 @@ $(function (event){
 //rename the Pictures to 0 1 2
 
     function setValue() {
+      console.log(playerScore);
       console.log(generateRandomForArray());
           $(".img img:last-child").remove() //i want the last monster to disappear
           var num = container[generateRandomForArray()];
