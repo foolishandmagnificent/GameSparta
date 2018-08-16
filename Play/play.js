40bde87c6781657bf3a38f3d6311b955f38157e8
Oceane <oceanelonneux@gmail.com>
2018-08-16 11:54:55 +0100

    almost done on player2

--- Play/play.js
+++ Play/play.js
@@ -6,18 +6,15 @@ $(function (event){
   var timer = $(".timer");
   var restart = $(".restart");
   var highScore = 0;
-  var playerscore = playerScore
-  var names = [];
-  //if statement to update highscore
-  var names = JSON.parse(localStorage.getItem('playerscore'));
-  if (names != null) {
-    highScore = names[names.length - 1];
-  }
-
+  var turn = 0; //who is playing
+  var playerscore = playerScore;
+  var score = $("score");
+  var firstName = $("firstName");
+  var secondName = $("secondName");
+  var score = JSON.parse(localStorage.getItem('playerscore'));
   var highScore = 0; //highscore
 
   function setValue() { //we're calling all the var we defined for the monsters
-    // clearInterval(monster);
     console.log(playerScore); //return the score of the player on the console
     generateRandomForArray();
         $(".img img:last-child").remove() //i want the last monster to disappear
@@ -34,15 +31,10 @@ $(function (event){
           "height": size + "px"
         });
     } //end of function setValue
-    //the monsters disappear after 1s
-
 
   $(".img").click(function() {
-    console.log("yes!")
-    // playerScore++; //raise score
     playerScore++; //IF you click on monster, the score rise up
     score.html("<b  class='playerscore'> Score: " + playerScore + " </b>");
-    // playerscore.html("<b  class='highscore'> High Score: " + highscore + " </b>");
     })//end enemy click function
 
   //random position values from top
@@ -73,44 +65,36 @@ $(function (event){
   var test = $("#test");
 
 
-  var turn = 0; //who is playing
   //i want to store the score of each player, compare them after timer
   var timeLeft = 20; //Im giving the player 20s to play
 
   var monstInt = setInterval(function() {
-    // console.log("running again and again  ")
     setValue();
   }, 800)
 
+  debugger;
+  $(".playerNames").html(firstName + " turn");
+
   var timerInterval = setInterval(function(){
     if (timeLeft != 0) {
-      // console.log(timeLeft);
       timeLeft--;
       $(".playerScore").html(playerScore); //show the score
       $("score").each(function(index){
         $(this).data("score", index)
-      }) //record the score
-      // setValue();
-      // })
-      // monster = setInterval(function(){
-      //   console.log("running again and again  ")
-      // }, 5000);
-      // $(".highScore").html(highScore); //show the score
+      })
+      $(".highScore").html(highScore); //show the score
 
     } else if (timeLeft == 0){
       console.log("end of game flow");
+      turn ++;
+      $(".playerNames").html(secondName + " turn");
       clearInterval(timerInterval)
       clearInterval(monstInt)
-        // stop();
-        // clearInterval(monstInt)
-      }//end of else if
+      }
     }, 1000); //end of set interval
 
       function stop() {
-        // $(".img img:last-child").remove() //remove image of last enemy
         console.log("finish");
-        // playGame = false;
-        // window.clearInterval(timerInterval);
       }
 
 
@@ -119,7 +103,7 @@ $(function (event){
     }
 
     // to restart the game
-    $("#restart").on("click", function restart() {
+    $("restart").on("click", function restart() {
       location.reload();
     })
   }); //  end of fonction event
---
+++ playerNames.js
@@ -0,0 +1,6 @@
+
+$("form").on("submit",function(event){
+  var firstName = $("input#firstName").val();
+  var secondName = $("input#secondName").val();
+  })
+// Use local storage and jason
